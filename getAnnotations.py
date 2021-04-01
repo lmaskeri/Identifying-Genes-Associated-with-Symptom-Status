75f979e049eaa4c356ca793dd53844efb3f31c2e
lmaskeri <79661805+lmaskeri@users.noreply.github.com>
2021-04-01 13:02:14 -0500

    comments

--- getAnnotations.py
+++ getAnnotations.py
@@ -6,12 +6,12 @@ os.getcwd()
 
 def getList(filename): #input is txt file of SRR accession numbers
     with open(filename) as f:
-        ECA = [line.rstrip() for line in f] #putting SRR numbers into a list
+        ECA = [line.rstrip() for line in f] #putting SRR numbers into a list 
     return ECA
 
 def getAnnotations(GCF):
     for i in GCF: #for each accession numbers
-        command ="./datasets download genome accession " + i + ".1 --exclude-rna  --filename " + i +".zip" #downloading the annotations f>
+        command ="./datasets download genome accession " + i + ".1 --exclude-rna  --filename " + i +".zip" #downloading the annotations for each accession number (strain) using the datasets tool from ncbi
         os.system(command)
         command2 = "unzip -o "+i+".zip -d Annotations"
         os.system(command2)
@@ -19,15 +19,16 @@ def getAnnotations(GCF):
 
 def moveAnnotations(GCF):
     for i in GCF:
-        command3 = "mv ./Annotations/ncbi_dataset/data/"+i+".1/"+i+"_ASM"+i[6:12]+"v1_genomic.fna Assemblies" #putting all assemblies  fo>
+        command3 = "mv ./Annotations/ncbi_dataset/data/"+i+".1/"+i+"_ASM"+i[6:12]+"v1_genomic.fna Assemblies" #putting all assemblies  for each accession number into an assemblies folder
         os.system(command3)
-        command4 = "mv ./Annotations/ncbi_dataset/data/"+ i+ ".1/protein.faa " + i + "_protein.faa" #renamed each protein sequence from t>
+        command4 = "mv ./Annotations/ncbi_dataset/data/"+ i+ ".1/protein.faa " + i + "_protein.faa" #renamed each protein sequence from the annotations retrieved from ncbi for each accession number
         os.system(command4)
-        command5 = "mv "+ i+ ".1/" + i + "_protein.faa ProteinSeqs" #moving the protein sequence file into a file called ProteinSeqs for >
+        command5 = "mv "+ i+ ".1/" + i + "_protein.faa ProteinSeqs" #moving the protein sequence file into a file called ProteinSeqs for all accession numbers
         os.system(command5)
 
 os.system("mkdir ProteinSeqs") #making a folder for the protein annotations for each accession number
-os.system("mkdir Assemblies") #making a folder for Assemblies
+os.system("mkdir Assemblies") #making a folder for Assemblies 
 
 getAnnotations(getList("EcoliAccessions.txt"))
 moveAnnotations(getList("EcoliAccessions.txt"))
+
