f2ca8518376a3a0750befe2bffaa28a96fcd679e
lmaskeri <79661805+lmaskeri@users.noreply.github.com>
2021-04-05 20:38:40 -0500

    Writing out to 5 matrices

--- generate_pres_abs_matrix.py
+++ generate_pres_abs_matrix.py
@@ -6,7 +6,15 @@ import csv
 
 acc_numbers = open("./ecoliAccessions.txt").read().split("\n") #pulling out accession numbers for each strain
 
-strain_numbers = open("./strainNumbers.txt").read().split("\n") #pulling out the strain numbers 
+strain_symptoms = []
+with open("./strain_symptom_samples.csv", "r") as in_file: #grabbing the strain # and symptom from the csv file
+    reader = csv.reader(in_file, delimiter = ',')
+    i = False
+    for row in in_file:
+        if i != False: #avoiding the first "Strain # and Participant Symtom" heading
+            strain_symptoms.append(row.strip("\n").split(",")) #gives ["strain_number", "Symtom status"] 
+        i = True
+        
 
 # Getting a list of only the homologous gene names
 take_out = [ "[Proteobacteria]", "[Escherichia]", "[Escherichia coli]", "[Enterobacterales]","[Enterobacteriaceae]", "[Bacteria]", "[Gammaproteobacteria]"] #Names for E.coli classes that we want to take out of the gene name
@@ -44,21 +52,66 @@ for acc in acc_numbers:
         pres_abs.append(strain_pres_abs) #appending the dictionary of presence absence values for that strain to the main presence absence list
     
 
-#Making the csv file in order to write the presence absence matrix out to
-with open("presence_absence.csv", "w", newline = "") as out_file:
-    writer = csv.writer(out_file)
-    #making the header with all of the homologous gene names
-    header = ["Strain Number"]
-    for gene in homologous_genes:
-        header.append(gene)
-    writer.writerow(header) #writing the header row to the csv file
-    for i in range(len(strain_numbers)): 
-        strain_row = [strain_numbers[i]] #placing the strain number as the first item in the row
-        strain_pres_abs = pres_abs[i] #pulling out the presence absence dictionary for this particular strain
-        for gene in homologous_genes: #for each gene in the homologous genes list
-            strain_row.append(strain_pres_abs[gene]) #placing the value for each homologous gene in the presence absence dictionary for this particular strain
-        writer.writerow(strain_row) #writing the presence absence information from this strain to the csv file as a row
-        
+'''
+Symptom Key:
+0 = No Lower Urinary Tract Symptoms (no LUTS)
+
+1 = Overactive bladder (OAB)
+
+2 = Urinary Tract Infection (UTI)
+
+3 = Urgency Urinary Incontinence (UUI)
+'''
+#opening the 5 csv files to write out to
+no_luts =  open("no_LUTS_pres_abs.csv", "w", newline = "")
+oab = open("OAB_pres_abs.csv", "w", newline = "") 
+uti = open("UTI_pres_abs.csv", "w", newline = "") 
+uui = open("UUI_pres_abs.csv", "w", newline = "")
+out_file =   open("full_presence_absence.csv", "w", newline = "")  
+
+#making csv writers for each to write to the file
+writer1 = csv.writer(no_luts)
+writer2 = csv.writer(oab)
+writer3 = csv.writer(uti)
+writer4 = csv.writer(uui)
+writer5 = csv.writer(out_file)
+
+#making the header with all of the homologous gene names
+header = ["Strain Number"]
+header.append("Strain Symptom")
+for gene in homologous_genes:
+    header.append(gene)
+
+#writing the header row to each csv file
+writer1.writerow(header)
+writer2.writerow(header)
+writer3.writerow(header)
+writer4.writerow(header)
+writer5.writerow(header)
+
+for i in range(len(strain_symptoms)): 
+    strain_row = [strain_symptoms[i][0]] #placing the strain number as the first item in the row
+    strain_row.append(strain_symptoms[i][1]) #placing the strain symptom associated with that strain from the data
+    strain_pres_abs = pres_abs[i] #pulling out the presence absence dictionary for this particular strain
+    for gene in homologous_genes: #for each gene in the homologous genes list
+        strain_row.append(strain_pres_abs[gene]) #placing the value for each homologous gene in the presence absence dictionary for this particular strain
+    writer5.writerow(strain_row) #writing the presence absence information from this strain to the csv file as a row
+    #Here I can now separate out the strains based on their symptom status and write out to separate matricies
+    if strain_symptoms[i][1] == "0":
+            writer1.writerow(strain_row)
+    if strain_symptoms[i][1] == "1":
+            writer2.writerow(strain_row)
+    if strain_symptoms[i][1] == "2":
+            writer3.writerow(strain_row)
+    if strain_symptoms[i][1] == "3":
+            writer4.writerow(strain_row)
+
+no_luts.close()
+oab.close()
+uti.close()
+uui.close()
+out_file.close()
+
 
 
 
