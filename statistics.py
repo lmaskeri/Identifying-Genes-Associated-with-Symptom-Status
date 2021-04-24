5fc5708e0b9b5923f7e9a022d1e4c7f5a4547e76
lmaskeri <79661805+lmaskeri@users.noreply.github.com>
2021-04-23 22:08:38 -0500

    Adding in matrix separation.

--- statistics.py
+++ statistics.py
@@ -1,3 +1,5 @@
+
+
 from Bio import SeqIO
 import csv
 import os
@@ -10,15 +12,13 @@ def transpose(file): #method for transposing the pres/abs matrix
     new_header = rez.iloc[0]
     rez =rez[1:]
     rez.columns =new_header #removing header made during the transposition and replacing with the first line of the matrix
-    transposed = open("transposed.csv","w")
-
-    writer = csv.writer(transposed)
-    rez.to_csv(transposed,index=False,header=True)
+    
+    return rez
 
 
-
-def kendall(names):
-    matrix = pd.read_csv("transposed.csv") #open transposed pres_abs matrix
+def kendall(names, matrix):
+    # matrix = pd.read_csv("transposed.csv") #open transposed pres_abs matrix
+    
     correlations = {} #initialize dictionary for keys as cluster names and values as correlation coefficients 
     correlations_p = {} #empty dictionary to hold p-values from correlation significance tests
 
@@ -40,7 +40,7 @@ def kendall(names):
     negative = k[v.index(min(v))] #find key with min value 
     results1.write("The clusters whose correlation coefficients are statistically significant are: \n")
     for key,value in correlations_p.items():
-        if value >=0.05: #significance level is 0.05
+        if value <=0.05: #significance level is 0.05
             results1.write(key + "\n") #if l-value above 0.05, reject null hypothesis, the correlation value is statistically significant and there is likely an association present
     results1.write("The cluster with the highest positive correlation to symptom status is " + positive + "\n") #key with max value is the highest positively correlated cluster
     results1.write("The cluster with the highest negative correlation to symptom status is " + negative) #key with min value is the highest negatively correlated cluster
@@ -49,9 +49,27 @@ def kendall(names):
         writer.writerow([key,value]) #write keys and values to a matrix in csv format
     results2.close()
     return correlations
+
+#pulling out the names of all clusters using file directory
 cluster_values = [] #empty list to append cluster filenames to
 for cluster_filename in os.listdir("./cluster_dir"):
     cluster_values.append(cluster_filename) #getting a list of cluster file names
     
-transpose("cluster_pres_abs_matrix.csv") #transpose matrix
-kendall(cluster_values) #run correlation tests
+r = transpose("cluster_pres_abs_matrix.csv") #transpose full matrix
+
+#Splitting the matrix up into each symptom with the added control no LUTS
+
+no_luts = r.loc[r['Strain Symptom'] == "NoLUTS"] #pulling out all of the control patient sample rows
+oab = r.loc[r['Strain Symptom'] == "OAB"] #pulling out all of the OAB patient sample rows
+uti = r.loc[r['Strain Symptom'] == "UTI"] #pulling out all of the UTI patient sample rows
+uui = r.loc[r['Strain Symptom'] == "UUI"] #pulling out all of the UUI patient sample rows
+
+# #combining a new dataframe with each symptom and the control group for correlation testing
+
+final_oab = pd.concat([oab, no_luts], ignore_index=True, sort=False)
+final_uti = pd.concat([uti, no_luts], ignore_index=True, sort=False)
+final_uui = pd.concat([uui, no_luts], ignore_index=True, sort=False)
+
+#running kendalls tau for each matrix
+kendall(cluster_values, final_oab)
+
